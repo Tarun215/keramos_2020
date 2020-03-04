142c0b40048d174ea52bec6e304fad4d00734c7e
Tarun215 <tarun.cer18@itbhu.ac.in>
2020-03-05 01:46:55 +0530

    added user router with routes to create, login, update, fetch, logout, delete user

---
+++ src/routers/user.js
@@ -0,0 +1,102 @@
+const express = require('express')
+const User = require('../db/models/user')
+const auth = require('../middlewares/auth')
+
+const router = new express.Router()
+
+
+// In comments ** means Authentication required
+
+//Route to create new user
+router.post('/users', async (req,res) => {
+    const user = new User(req.body)
+    
+    try{
+        await user.save()
+        const token = await user.genAuthToken()
+        res.status(201).send({ user, token })
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+//Route to login user
+router.post('/users/login', async(req, res) => {
+    try{
+        const user = await User.findByCredentials(req.body.email , req.body.password)
+        const token = await user.genAuthToken()
+        res.send({ user , token })
+    }catch(err) {   
+        res.status(400).send(`${err}`)
+    }
+})
+
+//Route to fetch user **
+router.get('/users/me', auth , async (req, res) => {
+    res.send(req.user)
+})
+
+//Route to update user info **
+router.patch('/users/me', auth , async (req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'email','password']
+    const isValidOperatioin = updates.every((update) => allowedUpdates.includes(update))
+    
+    if(!isValidOperatioin){
+        return res.status(400).send({ error : 'Invalid Updates'})
+    }
+    
+    try {
+        const user = req.user
+
+        updates.forEach((update) => user[update] = req.body[update])
+        await user.save()                                                              
+        
+        res.send(user)
+    } catch (err) {
+        res.status(400).send(err)
+    }
+})
+
+//Route to logout user from current session **
+router.post('/users/logout', auth , async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+            await req.user.save()
+            
+            res.send('Successfully logged out!!')
+        
+    } catch (err) {
+        res.status(500).send()
+    }
+})
+
+//Route to logout user form all sessions or all devices **
+router.post('/users/logoutAll', auth , async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+        res.send('Successfully logged out of all!!')
+    } catch (err) {
+        res.status(500).send()
+    }
+})
+
+//Route to delete user profile **
+router.delete('/users/me', auth , async (req, res) => {
+    try { 
+        await req.user.remove()
+        try{
+            Email.sendCancelationEmail(req.user.email, req.user.name)
+        } catch (err) { console.log('Unable to send Cancelation mail to user') }
+        res.send(req.user)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+
+
+module.exports = router
